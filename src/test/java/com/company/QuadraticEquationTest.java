3b0e41c499749403aa4c47492de0e84ce411fb64
Pavel Konovchenko <awakenidiot@gmail.com>
2020-04-27 23:25:30 +0300

    Patch Note - Added maven

---
+++ src/test/java/com/company/QuadraticEquationTest.java
@@ -0,0 +1,34 @@
+package com.company;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class QuadraticEquationTest {
+    @Test
+    public void testRegular() {
+        QuadraticEquation testing = new QuadraticEquation();
+        ReturningNumber expected = new ReturningNumber(-0.25, -1.0);
+        Assert.assertEquals(expected, testing.counting(4, 5, 1));
+    }
+
+    @Test
+    public void testNoRoots() {
+        QuadraticEquation testing = new QuadraticEquation();
+        ReturningNumber expected = new ReturningNumber(null,null);
+        Assert.assertEquals(expected, testing.counting(2,2,1));
+    }
+
+    @Test
+    public void testOneRoot() {
+        QuadraticEquation testing = new QuadraticEquation();
+        ReturningNumber expected = new ReturningNumber(-1.0,null);
+        Assert.assertEquals(expected, testing.counting(1,2,1));
+    }
+
+    @Test
+    public void testZeroes() {
+        QuadraticEquation testing = new QuadraticEquation();
+        ReturningNumber expected = new ReturningNumber(null,null);
+        Assert.assertEquals(expected, testing.counting(0,8,9));
+    }
+}
